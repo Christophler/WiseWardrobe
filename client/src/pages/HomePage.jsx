ac477d478446932ee961dd05732f40b4603f66ed
Christophler <chris.lpher15@gmail.com>
2026-03-27 23:31:48 -0400

    feat: Creating a default view of when the database is empty (no items were added).

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Container, Text, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW='container.xl' py={12}>
+      <VStack spacing={8}>
+        {/* The case of an empty database */}
+        <VStack spacing={2}>
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+						No items found 😢{" "}
+
+						<Link to={"/create"}>
+							<Text color={'blue.500'} _hover={{ textDecoration: "underline" }}>
+								Click here to add an item!
+							</Text>
+						</Link>
+					</Text>
+        </VStack>
+      </VStack>
+    </Container>
   )
 }
 
