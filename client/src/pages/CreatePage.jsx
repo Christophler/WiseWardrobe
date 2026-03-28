d5839ae56396057f55a9edf5b8be6b4c869c5d33
Christophler <chris.lpher15@gmail.com>
2026-03-27 23:10:09 -0400

    feat: Creating the functionality to add a new item to the database.
    - Currently console logs the status of the request.

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from '@chakra-ui/react';
+import { useState } from 'react'
+import { useProductStore } from '../store/product';
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
+  const [newItem, setNewItem] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const {createProduct}=useProductStore()
+
+  const handleAddItem = async() => {
+    const {success, message} = await createProduct(newItem);
+    console.log("Success: ", success);
+    console.log("Message: ", message);
+  }
+
+  return <Container maxW={"container.sm"} pt={5}>
+    <VStack
+      spacing={8}
+    >
+      <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+        Add New Item
+      </Heading>
+
+      <Box
+        w={"full"} bg={useColorModeValue("white", "gray.800")}
+        p={6} rounded={"lg"} shadow={"md"}
+      >
+        <VStack spacing={4}>
+						<Input
+							placeholder='Item Name'
+							name='name'
+							value={newItem.name}
+							onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+						/>
+						<Input
+							placeholder='Price'
+							name='price'
+							type='number'
+							value={newItem.price}
+							onChange={(e) => setNewItem({ ...newItem, price: e.target.value })}
+						/>
+						<Input
+							placeholder='Image URL'
+							name='image'
+							value={newItem.image}
+							onChange={(e) => setNewItem({ ...newItem, image: e.target.value })}
+						/>
+
+            <Button colorScheme='blue' onClick={handleAddItem} w={'full'}>
+              Add Item
+            </Button>
+        </VStack>
+      </Box>
+    </VStack>
+  </Container>
 }
 
 export default CreatePage
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
+
+const API_PORT = process.env.EXPRESS_API_PORT || '5000';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api":{
+        target:`http://localhost:${API_PORT}`,
+        changeOrigin: true
+      }
+    }
+  }
 })
