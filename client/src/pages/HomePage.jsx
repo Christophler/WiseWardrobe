acea1841a540c2609deac2c54926d5a24499fa71
Christophler <chris.lpher15@gmail.com>
2026-03-28 00:14:11 -0400

    feat: Added a product card element to display the database records.
    - Displays the name, price, and image entered. (If the image address is not valid, the default will be the product's name)
    - Each product card includes icons for editing and deleting the record. Full functionality for these has yet to be implemented yet.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
-import { Container, Text, VStack } from '@chakra-ui/react'
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useProductStore } from '../store/product'
+import ProductCard from '../components/ProductCard'
 
 const HomePage = () => {
   // Fetching the items from the database
@@ -9,7 +10,6 @@ const HomePage = () => {
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
-  console.log("products:", products);
 
   return (
     <Container maxW='container.xl' py={12}>
@@ -25,6 +25,22 @@ const HomePage = () => {
 				>
           Current Items⚡
         </Text>
+
+        {/* Grid layout view for the items in the database */}
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {/* Mapping the product card(s) to the grid */}
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
 
         {/* The case of an empty database */}
         <VStack spacing={2}>
